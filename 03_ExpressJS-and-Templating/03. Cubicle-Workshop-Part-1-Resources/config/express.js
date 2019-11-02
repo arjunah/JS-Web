162d6e717c1421df05fca69ee20edbedf7750727
arjunah <arjunah@abv.bg>
2019-11-02 20:16:07 +0200

    partials

--- 03_ExpressJS-and-Templating/03. Cubicle-Workshop-Part-1-Resources/config/express.js
+++ 03_ExpressJS-and-Templating/03. Cubicle-Workshop-Part-1-Resources/config/express.js
@@ -11,5 +11,5 @@ module.exports = (app) => {
 
     app.engine(".hbs", handlebars({ extname: ".hbs"}));
     app.set("view engine", ".hbs");
-    app.locals.layout = false; // disable layouts
+    // app.locals.layout = false; // disable layouts
 };
